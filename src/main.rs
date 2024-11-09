3b242226414dbac38699b5503f8160a6ccec163a
Alexander Dadiani <alexdadiani1994@gmail.com>
2024-11-09 22:28:07 +0100

    Add list parsing
    do proper parsing of int and string - rather than splitting on ':' and
    stripping the suff/pre fixes, iterate over the chars and grab whats
    necessary

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::{bail, Context, Result};
 use clap::{Parser, Subcommand};
 
-const BENCODE_STRING_SPLIT_CHAR: &str = ":";
-const BENCODE_INT_PREFIX: &str = "i";
-const BENCODE_INT_SUFIX: &str = "e";
+const BENCODE_STRING_SPLIT_CHAR: char = ':';
+const BENCODE_INT_PREFIX: char = 'i';
+const BENCODE_INT_SUFFIX: char = 'e';
+const BENCODE_LIST_PREFIX: char = 'l';
+const BENCODE_LIST_SUFFIX: char = 'e';
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -31,6 +33,11 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+struct ParsedValue {
+    length: usize,
+    value: String,
+}
+
 enum BencodeType {
     String,
     Number,
@@ -41,9 +48,18 @@ enum BencodeType {
 impl BencodeType {
     fn new(input: &String) -> BencodeType {
         match input {
+            _ if input.starts_with(BENCODE_INT_PREFIX) && input.ends_with(BENCODE_INT_SUFFIX) => BencodeType::Number,
+            _ if input.starts_with(BENCODE_LIST_PREFIX) && input.ends_with(BENCODE_LIST_SUFFIX) => BencodeType::List,
             _ if input.contains(BENCODE_STRING_SPLIT_CHAR) => BencodeType::String,
-            _ if input.starts_with("i") && input.ends_with("e") => BencodeType::Number,
-            _ if input.starts_with("l") && input.ends_with("e") => BencodeType::List,
+            _ => BencodeType::Invalid,
+        }
+    }
+
+    fn identify_char(input: &char) -> BencodeType {
+        match input {
+            _ if input.is_numeric() => BencodeType::String,
+            _ if *input == BENCODE_INT_PREFIX => BencodeType::Number,
+            _ if *input == BENCODE_LIST_PREFIX => BencodeType::List,
             _ => BencodeType::Invalid,
         }
     }
@@ -55,41 +71,128 @@ fn decode(input: &String) -> Result<String> {
         BencodeType::String => bdecode_string(input)?,
         BencodeType::Number => bdecode_num(input)?,
         BencodeType::List => bdecode_list(input)?,
-        BencodeType::Invalid => bail!("dont know how to handle {input}")
+        BencodeType::Invalid => bail!("dont know how to handle {input}"),
     };
 
-    Ok(res)
+    Ok(res.value)
 }
 
-fn bdecode_list(input: &String) -> Result<String> {
-    todo!()
+fn bdecode_list(input: &str) -> Result<ParsedValue> {
+    // encoded like l5:helloi52ee
+    let mut elements = &input[1..input.len() - 1];
+
+    let mut list: Vec<String> = Vec::new();
+
+    let mut elem_iter = elements.chars();
+    let mut step = 0;
+    loop {
+        let char = match elem_iter.nth(step) {
+            Some(x) => x,
+            None => break,
+        };
+
+        let next_type = BencodeType::identify_char(&char);
+
+        let res = match next_type {
+            BencodeType::String => bdecode_string(elements)?,
+            BencodeType::Number => bdecode_num(elements)?,
+            BencodeType::List => bdecode_list(elements)?,
+            BencodeType::Invalid => bail!("dont know how to handle {input}"),
+        };
+
+        list.push(res.value);
+        step = res.length - 1;
+        elements = match elements.get(res.length..elements.len()) {
+            Some(slice) => slice,
+            None => break,
+        };
+    }
+
+    Ok(ParsedValue {
+        length: input.len(),
+        value: serde_json::to_string(&list)?,
+    })
 }
 
-fn bdecode_string(input: &String) -> Result<String> {
+fn bdecode_string(input: &str) -> Result<ParsedValue> {
     // encoded like <length:contents>
     let mut split = input.splitn(2, BENCODE_STRING_SPLIT_CHAR);
 
-    let _ = split.next().ok_or_else(|| {
+    let length_string = split.next().ok_or_else(|| {
         anyhow::anyhow!("length missing before {BENCODE_STRING_SPLIT_CHAR}: input: {input}")
     })?;
+
+    let length = length_string
+        .parse()
+        .context(format!("parsing length in encoded string {input}"))?;
+
     let contents = split.next().ok_or_else(|| {
         anyhow::anyhow!("content missing after {BENCODE_STRING_SPLIT_CHAR}: input: {input}")
     })?;
 
-    Ok(serde_json::to_string(contents)?)
+    let relevant_content = match contents.get(0..length) {
+        Some(slice) => slice,
+        None => bail!(
+            "incorrect length encoding! Expected {length} characters, but have these {contents}"
+        ),
+    };
+
+    let len = relevant_content.len() + length_string.len() + 1;
+
+    Ok(ParsedValue {
+        length: len,
+        value: serde_json::to_string(&relevant_content)?,
+    })
 }
 
-fn bdecode_num(input: &String) -> Result<String> {
+fn bdecode_num(input: &str) -> Result<ParsedValue> {
     // endcoded like i<number>e. Number can be negative.
-    let num_string = input
-        .strip_prefix(BENCODE_INT_PREFIX)
-        .ok_or_else(|| anyhow::anyhow!("expected prefix {BENCODE_INT_PREFIX} missing"))?
-        .strip_suffix(BENCODE_INT_SUFIX)
-        .ok_or_else(|| anyhow::anyhow!("expected suffix {BENCODE_INT_SUFIX} missing"))?;
+
+    let mut input_chars = input.chars();
+    let mut num_string = String::new();
+    let mut signed_seen = false;
+
+    while let Some(ch) = input_chars.next() {
+        match ch {
+            BENCODE_INT_PREFIX => continue,
+            BENCODE_INT_SUFFIX => break,
+            _ if ch.is_numeric() || (ch == '-' && !signed_seen) => {
+                num_string.push(ch);
+                signed_seen = true;
+                continue;
+            }
+            _ => bail!("unexpected char '{ch}' in number input '{input}'"),
+        }
+    }
 
     let num: i64 = num_string
         .parse()
         .context(format!("cannot parse {num_string} as i64"))?;
 
-    Ok(serde_json::to_string(&num)?)
+    let len = num_string.len() + 2; // prefix and suffix was stripped.
+
+    Ok(ParsedValue {
+        length: len,
+        value: serde_json::to_string(&num)?,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bdecode_list() {
+        let input = String::from("l5:helloi52ee");
+        let res = bdecode_list(&input);
+        assert_eq!(res.unwrap().value, "[\"\\\"hello\\\"\",\"52\"]");
+
+        let input = String::from("l5:helloi52ei43ee");
+        let res = bdecode_list(&input);
+        assert_eq!(res.unwrap().value, "[\"\\\"hello\\\"\",\"52\",\"43\"]");
+
+        let input = String::from("l5:helloi52ei43e4:adade");
+        let res = bdecode_list(&input);
+        assert_eq!(res.unwrap().value, "[\"\\\"hello\\\"\",\"52\",\"43\",\"\\\"adad\\\"\"]");
+    }
 }
