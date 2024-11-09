a283ba352a90f5c0b808aa63bb895f4adc3d282b
Alexander Dadiani <alexdadiani1994@gmail.com>
2024-11-09 20:46:47 +0100

    add int decode fn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use anyhow::{bail, Context, Result};
 use clap::{Parser, Subcommand};
-use anyhow::Result;
 
-const BENCODE_SPLIT_CHAR: &str = ":";
+const BENCODE_STRING_SPLIT_CHAR: &str = ":";
+const BENCODE_INT_PREFIX: &str = "i";
+const BENCODE_INT_SUFIX: &str = "e";
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -12,36 +14,56 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Decode{input: String}
+    Decode { input: String },
 }
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match &cli.command {
-    Some(Commands::Decode { input }) => {
-        decode_command(input)?
-    }
-    None => {}
+        Some(Commands::Decode { input }) => decode(input)?,
+        None => {}
     };
 
     Ok(())
 }
 
-fn decode_command(input: &String) -> Result<()>{
-    let decoded_json = bdecode_string(input)?;
-    println!("{}", decoded_json );
+fn decode(input: &String) -> Result<()> {
+    let res = match input {
+        _ if input.contains(BENCODE_STRING_SPLIT_CHAR) => bdecode_string(input)?,
+        _ if input.starts_with("i") && input.ends_with("e") => bdecode_int(input)?,
+        _ => bail!("cannot decode unknown format {input}"),
+    };
+
+    println!("{}", res);
     Ok(())
 }
 
 fn bdecode_string(input: &String) -> Result<String> {
     // encoded like <length:contents>
-    let mut split = input.splitn(2, BENCODE_SPLIT_CHAR);
+    let mut split = input.splitn(2, BENCODE_STRING_SPLIT_CHAR);
 
-    let _ = split.next().ok_or_else(|| anyhow::anyhow!("length missing before {BENCODE_SPLIT_CHAR}: input: {input}"))?;
-    let contents = split.next().ok_or_else(|| anyhow::anyhow!("content missing after {BENCODE_SPLIT_CHAR}: input: {input}"))?;
-
+    let _ = split.next().ok_or_else(|| {
+        anyhow::anyhow!("length missing before {BENCODE_STRING_SPLIT_CHAR}: input: {input}")
+    })?;
+    let contents = split.next().ok_or_else(|| {
+        anyhow::anyhow!("content missing after {BENCODE_STRING_SPLIT_CHAR}: input: {input}")
+    })?;
 
     Ok(serde_json::to_string(contents)?)
 }
 
+fn bdecode_int(input: &String) -> Result<String> {
+    // endcoded like i<number>e. Number can be negative.
+    let num_string = input
+        .strip_prefix(BENCODE_INT_PREFIX)
+        .ok_or_else(|| anyhow::anyhow!("expected prefix {BENCODE_INT_PREFIX} missing"))?
+        .strip_suffix(BENCODE_INT_SUFIX)
+        .ok_or_else(|| anyhow::anyhow!("expected suffix {BENCODE_INT_SUFIX} missing"))?;
+
+    let num: i64 = num_string
+        .parse()
+        .context(format!("cannot parse {num_string} as i64"))?;
+
+    Ok(serde_json::to_string(&num)?)
+}
